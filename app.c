a4deced0b13977fe93d2464c577c08bb5d756c12
kali <kali@kaliakka>
2022-02-08 19:31:51 +0100

    premier commit

---
+++ app.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int square(int a) {
+	return a * a;
+}
+
+
+int main() {
+
+	printf("%s", "Simple programme en C\n");
+  printf("%s", " .... pour apprendre le CI/CD ... avec Jenkins");
+  printf("\nLe resultat de 4 au carre est %d", square(4));
+
+	return 0;
+}
